15e939622921d74384541896a8357c53f712c649
BubaInc <neumeier.nicolas@outlook.com>
2025-03-01 12:10:34 +0100

    autokollomation einfachlinsen

--- utils/fehler_gauss.py
+++ utils/fehler_gauss.py
@@ -1,14 +1,15 @@
 import numpy as np
 from sympy import lambdify, diff
 
-def fehler_gauss(formula, dependent_symbols, values, uncertainties):
+def fehler_gauss(formula, dependent_symbols, values, uncertainties, print_errors=False):
     u_formula = 0
     formula_l = lambdify(dependent_symbols, formula)
     for i, s in enumerate(dependent_symbols):
         derivative = diff(formula, s)
         derivative_l = lambdify(dependent_symbols, derivative)
         _u = derivative_l(*values) ** 2 * uncertainties[i] ** 2
-        print(s, _u)
+        if print_errors:
+            print(s, _u)
         u_formula += _u
 
     return formula_l(*values), np.sqrt(u_formula)

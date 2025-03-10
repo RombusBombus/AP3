a80dd51caf71fe32fee6dc34417013ab885f6979
RombusBombus <neumeier.nicolas@outlook.com>
2025-03-10 20:03:31 +0100

    rhioiuhe

--- utils/fehler_gauss.py
+++ utils/fehler_gauss.py
@@ -16,18 +16,24 @@ def fehler_gauss(formula, dependent_symbols, values, uncertainties, print_errors
     return formula_l(*values), np.sqrt(u_formula)
 
 
-
 def weighted_mean(measurements, uncertainties):
-
     weights = 1 / uncertainties**2
-
     weighted_mean = np.dot(measurements, weights) / weights.sum()
-
     n = measurements.shape[0]
-
-    internal_uncertainty = np.sqrt(1/weights.sum())
-    external_uncertainty = np.sqrt(np.dot(weights, (measurements - weighted_mean)**2) / ((n-1)*weights.sum()))
-
+    internal_uncertainty = np.sqrt(1 / weights.sum())
+    external_uncertainty = np.sqrt(
+        np.dot(weights, (measurements - weighted_mean) ** 2) / ((n - 1) * weights.sum())
+    )
     uncertainty = max(internal_uncertainty, external_uncertainty)
-
     return weighted_mean, uncertainty
+
+
+def format_result(vals, us, exponents, digits, unit):
+    results = []
+    for val, u, exponent, digit in zip(vals, us, exponents, digits):
+        val_fmt = str(np.round(val * 10**exponent, digit))
+        u_fmt = str(np.round(u * 10**exponent, digit))
+        results.append(
+            r"\SI{" + val_fmt + f"({u_fmt[-digit:]})e{-exponent}" + r"}{" + unit + r"}"
+        )
+    return results
